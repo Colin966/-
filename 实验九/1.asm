b46af6a835d6b7d4e36ee99ab5b0141812428052
Colin_fdp <84764850+Colin966@users.noreply.github.com>
2023-09-16 15:35:43 +0800

    Add files via upload

--- 实验九/1.asm
+++ 实验九/1.asm
@@ -1,49 +1,49 @@
-assume cs:codesg,ds:datasg
+assume cs:codesg,ds:datasg,ss:stacksg
 datasg segment
 	db 'welcome to masm!'
+	db 00000010b,00100100b,01110001b
 datasg ends
 
+stacksg segment
+	dw 3 dup(0)
+	stacksg ends
+	
 codesg segment
-start:	mov ax,datasg
+start:	mov ax,stacksg
+		mov ss,ax
+		mov sp,6h
+		
+		mov ax,datasg
 		mov ds,ax
-		mov bx,0					
+		mov bx,0
+		mov di,10h
 				
-		mov ax,0b800h			
-		mov es,ax					
-		mov si,0b40h				;定位到要显示字符串的段地址和偏移地址(计算得出)
+		mov ax,0b800h
+		mov es,ax
+		mov si,720h
 		
-		mov cx,7
+			
+		mov cx,3
 		
-	s1:	mov al,byte ptr ds:[bx]		;按字节从数据段中取出字符串,放到ax低位
-		mov ah,00000010b			;在ax高位中放入字符串颜色属性
-		inc bx
-		mov es:[si],ax				;将ax放到显存中，小端，高位属性放到高地址，低位字符放到低地址
-		add si,2
-		loop s1						;循环7次把'welcome'打印出来
+	s2:	push cx
+		push si
+		push bx
 		
-		inc bx
-		add ax,2					;跳过空格
+		mov cx,16
 		
-		mov cx,2
-		
-	s2:	mov al,byte ptr ds:[bx]
-		mov ah,00100100b
-		inc bx
+	s1:	mov al,byte ptr ds:[bx]
+		mov ah,byte ptr ds:[di]
 		mov es:[si],ax
-		add si,2
-		loop s2						;循环2次把'to'打印出来
-		
 		inc bx
-		add ax,2					;跳过空格
-		
-		mov cx,5
-		
-	s3:	mov al,byte ptr ds:[bx]
-		mov ah,01110001b
-		inc bx
-		mov es:[si],ax
 		add si,2
-		loop s3						;循环5次把'masm!'打印出来
+		loop s1
+		
+		inc di
+		pop bx
+		pop si
+		add si,0a0h
+		pop cx		
+		loop s2
 		
 		mov ax,4c00h
 		int 21h
